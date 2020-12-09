21026d271b4c5bbf1575a3df2475fd5a73a33c76
=Wojciech Kowalczyk <wojtaz@vp.pl>
2020-12-09 16:18:14 +0100

    09.12

--- kwerendy_SQL_kowalczykw_lab08.sql
+++ kwerendy_SQL_kowalczykw_lab08.sql
@@ -29,3 +29,63 @@ END
 
 //
 DELIMITER;
+
+PKT #3
+1) 
+DELIMITER //
+CREATE PROCEDURE eliksir_sily(IN id int)
+BEGIN
+Update kreatura set udzwig = 1.2 * udzwig where idKreatury = id;
+END
+//
+DELIMITER ;
+
+2) 
+DELIMITER //
+CREATE FUNCTION duze_znaki()     
+RETURNS VARCHAR (255)
+BEGIN     
+DECLARE tekst VARCHAR(255);
+    
+SELECT Upper(tekst) INTO @tekst;  
+ RETURN @tekst; 
+END //
+DELIMITER ;
+
+select duze_znaki();
+
+PKT #4
+1)
+DELIMITER $$
+CREATE TRIGGER sprawdz_tesciowa
+AFTER INSERT ON wyprawa
+FOR EACH ROW
+BEGIN
+DECLARE zmienna INT;
+SELECT count(*) INTO zmienna
+FROM etapy_wyprawy AS e, kreatura AS k, wyprawa AS w, uczestnicy AS u
+WHERE e.idWyprawy=w.id_wyprawy AND k.idKreatury=u.id_uczestnika
+AND u.id_wyprawy=w.id_wyprawy
+AND k.nazwa='Tesciowa' AND e.sektor=7
+AND w.id_wyprawy=NEW.id_wyprawy;
+IF zmienna > 0
+THEN
+INSERT INTO system_alarmowy VALUES(DEFAULT, 'Tesciowa nadchodzi !!!');
+END IF;
+END
+$$
+DELIMITER;
+2) (nie robić)
+
+
+PKT #5
+1)
+DELIMITER //
+CREATE PROCEDURE udzwigi(OUT sr FLOAT, OUT su FLOAT, OUT max FLOAT)
+BEGIN
+SELECT AVG(udzwig) , SUM(udzwig) , MAX(udzwig) INTO sr, su, max from kreatura;
+END
+//
+DELIMITER ;
+2)
+
