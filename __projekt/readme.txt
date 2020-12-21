e37ba11adb1925d872c85cf7185eae37f8c009a0
=Wojciech Kowalczyk <wojtaz@vp.pl>
2020-12-21 15:41:40 +0100

    projekt i pliki z cw

---
+++ __projekt/readme.txt
@@ -0,0 +1,5 @@
+Wojciech Kowalczyk 
+g 2 II ISI
+Projekt zaliczeniowy 
+Bazy danych
+Serwis komputerowy
--- kwerendy_SQL_kowalczykw_cw_cz3_github.sql
+++ kwerendy_SQL_kowalczykw_cw_cz3_github.sql
@@ -7,6 +7,6 @@ kowalczykw Ćwiczenia cz.3 github
 5)	select  k.nazwa_kategori,  group_concat(t.nazwa_towaru) nazwa_towaru  from kategoria k, towar t where k.id_kategori = t.kategoria group by k.nazwa_kategori;
 6&7) select id_pracownika, imie, nazwisko, round(avg(pensja),2) srednia_pensja from pracownik  where (year(current_date)-year(data_zatrudnienia)) >=5 group by id_pracownika;
 8)	select t.nazwa_towaru, sum(p.ilosc) suma_sprzedanych from pozycja_zamowienia  p, towar t where t.id_towaru=p.towar GROUP BY towar order by suma_sprzedanych desc limit 10;
-9)	
-10)	
+9)	SELECT z.numer_zamowienia, SUM(po.ilosc*po.cena) AS wartosc FROM zamowienie AS z, pozycja_zamowienia AS po WHERE z.data_zamowienia BETWEEN '2017-01-01' AND '2017-03-31' GROUP BY z.numer_zamowienia;
+10)	select p.imie, p.nazwisko, sum(pz.cena*pz.ilosc) as suma_wartosci from zamowienie z, pozycja_zamowienia pz, pracownik p where z.pracownik_id_pracownika=p.id_pracownika and pz.zamowienie=z.id_zamowienia group by z.pracownik_id_pracownika order by suma_wartosci desc;
 	
